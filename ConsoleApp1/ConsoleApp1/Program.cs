f0388b719ac3dbb1068a4c0ee14d9a1782d5d864
MaithiliJadhav16 <125574386+MaithiliJadhav16@users.noreply.github.com>
2023-02-24 09:32:20 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        for (int input = 1; input <= 10; input++)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
